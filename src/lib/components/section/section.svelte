4b431771ab39de9106f8240ecf4f24554d9d8033
jcebrian <jcebrian@mail.com>
2024-04-11 22:56:58 +0200

    Change section triangle and padding to center text and icon

--- src/lib/components/section/section.svelte
+++ src/lib/components/section/section.svelte
@@ -1,80 +1,80 @@
 <script>
-    export let title = "";
-    export let iconUrl = "";
-    export let reverse = false;
+  export let title = "";
+  export let iconUrl = "";
+  export let reverse = false;
 </script>
 
 <div class="section">
-    <div class="title-container" class:reverse>
-        <div class="title">{title}</div>
-        <div class="icon" style="background-image: url({iconUrl})"></div>
-    </div>
+  <div class="title-container" class:reverse>
+    <div class="title">{title}</div>
+    <div class="icon" style="background-image: url({iconUrl})"></div>
+  </div>
 
-    <div class="content">
-        <slot />
-    </div>
+  <div class="content">
+    <slot />
+  </div>
 </div>
 
 <style>
-    .section {
-        display: flex;
-        flex-direction: column;
-        gap: var(--section-title-content-gap);
-    }
-    .title-container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border-bottom: 2px solid var(--border-color);
-        border-collapse: collapse;
-        position: relative;
-        padding: 0 var(--section-padding);
-    }
+  .section {
+    display: flex;
+    flex-direction: column;
+    gap: var(--section-title-content-gap);
+  }
+  .title-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 2px solid var(--border-color);
+    border-collapse: collapse;
+    position: relative;
+    padding: 0.2rem var(--section-padding);
+  }
 
-    .content {
-        padding: 0 var(--section-padding);
-        display: flex;
-        flex-direction: column;
-        gap: var(--section-inner-gap);
-    }
+  .content {
+    padding: 0 var(--section-padding);
+    display: flex;
+    flex-direction: column;
+    gap: var(--section-inner-gap);
+  }
 
-    .title-container.reverse {
-        flex-direction: row-reverse;
-    }
+  .title-container.reverse {
+    flex-direction: row-reverse;
+  }
 
-    .title-container::before {
-        display: block;
-        position: absolute;
-        width: 0;
-        height: 0;
-        border-top: 6px solid transparent;
-        border-bottom: 6px solid transparent;
-        border-left: 6px solid var(--color-primary);
-        top: 43px;
-        left: 0px;
-        content: "";
-    }
-    .title-container.reverse::before {
-        right: 0px;
-        left: auto;
-    }
+  .title-container::before {
+    display: block;
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-top: 8px solid transparent;
+    border-bottom: 8px solid transparent;
+    border-left: 8px solid var(--color-primary);
+    bottom: calc(0rem - 0.2rem - 5px);
+    left: 0px;
+    content: "";
+  }
+  .title-container.reverse::before {
+    right: 0px;
+    left: auto;
+  }
 
-    .title-container.reverse::before {
-        rotate: 180deg;
-    }
+  .title-container.reverse::before {
+    rotate: 180deg;
+  }
 
-    .title-container * {
-        color: var(--color-primary);
-        font-family: "League Spartan";
-        font-weight: 600;
-        font-size: 2rem;
-        text-transform: uppercase;
-    }
+  .title-container * {
+    color: var(--color-primary);
+    font-family: "League Spartan";
+    font-weight: 600;
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
 
-    .icon {
-        background-size: contain;
-        background-repeat: no-repeat;
-        height: 3rem;
-        width: 3rem;
-    }
+  .icon {
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 3rem;
+    width: 3rem;
+  }
 </style>
