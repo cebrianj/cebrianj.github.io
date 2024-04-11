d1b7fcf8a7b6740618ccd01010d7fe07ed403081
jcebrian <jcebrian@mail.com>
2024-04-11 23:34:28 +0200

    Add projects section

---
+++ src/lib/components/projects/projects.svelte
@@ -0,0 +1,71 @@
+<div class="projects-container">
+  <div class="card glass-bg project">
+    <div class="project-image skeleton"></div>
+    <p class="project-title skeleton"></p>
+    <p class="project-description skeleton"></p>
+  </div>
+  <div class="card glass-bg project">
+    <div class="project-image skeleton"></div>
+    <p class="project-title skeleton"></p>
+    <p class="project-description skeleton"></p>
+  </div>
+  <div class="card glass-bg project">
+    <div class="project-image skeleton"></div>
+    <p class="project-title skeleton"></p>
+    <p class="project-description skeleton"></p>
+  </div>
+</div>
+
+<style>
+  .projects-container {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    gap: var(--section-inner-gap);
+    color: var(--color-primary);
+  }
+
+  .project {
+    flex: 1 1 300px;
+    max-width: 300px;
+    height: 250px;
+    overflow: hidden;
+  }
+
+  .project-image {
+    width: 100%;
+    height: 100px;
+    position: absolute;
+    margin-top: calc(0px - var(--card-padding-y));
+    margin-left: calc(0px - var(--card-padding-x));
+    margin-right: calc(0px - var(--card-padding-x));
+  }
+
+  .project-title {
+    margin-top: 100px;
+    text-align: center;
+    width: 100%;
+    height: 2em;
+  }
+
+  .project-description {
+    margin-top: 20px;
+    text-align: center;
+    width: 100%;
+    height: 3.3em;
+  }
+
+  .skeleton {
+    -webkit-animation: skeleton-loading 1s linear infinite alternate;
+    animation: skeleton-loading 1s linear infinite alternate;
+  }
+
+  @keyframes skeleton-loading {
+    0% {
+      background-color: var(--color-primary-50-opacity);
+    }
+    100% {
+      background-color: var(--color-primary-25-opacity);
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,39 @@
 <script>
-    import About from "$lib/components/about/about.svelte";
-    import Experience from "$lib/components/experience/experience.svelte";
-    import Navbar from "$lib/components/navbar/navbar.svelte";
-    import Section from "$lib/components/section/section.svelte";
-    import Skills from "$lib/components/skills/skills.svelte";
-    import "$lib/styles/_index";
+  import About from "$lib/components/about/about.svelte";
+  import Experience from "$lib/components/experience/experience.svelte";
+  import Navbar from "$lib/components/navbar/navbar.svelte";
+  import Projects from "$lib/components/projects/projects.svelte";
+  import Section from "$lib/components/section/section.svelte";
+  import Skills from "$lib/components/skills/skills.svelte";
+  import "$lib/styles/_index";
 </script>
 
 <Navbar></Navbar>
 <div class="content">
-    <Section title="About" iconUrl="icons/icon-user.svg">
-        <About></About>
-    </Section>
+  <Section title="About" iconUrl="icons/icon-user.svg">
+    <About></About>
+  </Section>
 
-    <Section title="Skills" iconUrl="icons/icon-skills.svg" reverse="true">
-        <Skills></Skills>
-    </Section>
+  <Section title="Skills" iconUrl="icons/icon-skills.svg" reverse="true">
+    <Skills></Skills>
+  </Section>
 
-    <Section title="Experience" iconUrl="icons/icon-experience.png">
-        <Experience></Experience>
-    </Section>
+  <Section title="Experience" iconUrl="icons/icon-experience.png">
+    <Experience></Experience>
+  </Section>
+
+  <Section title="Projects" iconUrl="icons/icon-code.png" reverse="true">
+    <Projects></Projects>
+  </Section>
 </div>
 
 <style>
-    .content {
-        display: flex;
-        flex-direction: column;
-        gap: var(--sections-gap);
-        max-width: var(--content-max-width);
-        margin: auto;
-        padding: var(--padding-big) var(--padding-small);
-    }
+  .content {
+    display: flex;
+    flex-direction: column;
+    gap: var(--sections-gap);
+    max-width: var(--content-max-width);
+    margin: auto;
+    padding: var(--padding-big) var(--padding-small);
+  }
 </style>
