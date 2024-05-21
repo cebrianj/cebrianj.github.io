56f7cdd11625061c2d55f433ab4c8fd3236a16f8
cebrianj <cebrianj@gmail.com>
2024-05-21 23:32:07 +0200

    Add information in projects section

--- src/lib/components/projects/projects.svelte
+++ src/lib/components/projects/projects.svelte
@@ -1,14 +1,39 @@
 <div class="projects-container">
   <div class="card glass-bg project">
-    <div class="project-image skeleton"></div>
-    <p class="project-title skeleton"></p>
-    <p class="project-description skeleton"></p>
+    <div class="project-image">
+      <img src="projects/portfolio.png" />
+    </div>
+    <p class="project-title">
+      PORTFOLIO <a
+        class="icon github"
+        href="//github.com/cebrianj/cebrianj.github.io"
+      >
+      </a>
+    </p>
+    <p class="project-description">
+      A dynamic website developed with Svelte, serving as my ever-evolving
+      portfolio where I will share my projects and achievements
+    </p>
   </div>
+
   <div class="card glass-bg project">
-    <div class="project-image skeleton"></div>
-    <p class="project-title skeleton"></p>
-    <p class="project-description skeleton"></p>
+    <div class="project-image">
+      <img class="no-crop" src="projects/minesweeper.png" />
+    </div>
+    <p class="project-title">
+      MINESWEEPER <a
+        class="icon github"
+        href="//https://github.com/cebrianj/Minesweeper"
+      >
+      </a>
+    </p>
+    <p class="project-description">
+      Python Minesweeper project showcases modular development, terminal UI
+      creation, extensible design, and advanced algorithms, emphasizing
+      separation of game logic and visual display
+    </p>
   </div>
+
   <div class="card glass-bg project">
     <div class="project-image skeleton"></div>
     <p class="project-title skeleton"></p>
@@ -26,10 +51,15 @@
   }
 
   .project {
-    flex: 1 1 300px;
-    max-width: 300px;
-    height: 250px;
+    flex: 1 1 350px;
+    max-width: 350px;
+    height: 300px;
     overflow: hidden;
+    transition: all 1s;
+  }
+
+  .project:hover {
+    transform: scale(1.1);
   }
 
   .project-image {
@@ -39,20 +69,55 @@
     margin-top: calc(0px - var(--card-padding-y));
     margin-left: calc(0px - var(--card-padding-x));
     margin-right: calc(0px - var(--card-padding-x));
+    overflow: hidden;
+  }
+
+  .project-image img {
+    position: absolute;
+    width: 100%;
+    top: -50%;
+  }
+
+  .project-image img.no-crop {
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    top: 0;
   }
 
   .project-title {
-    margin-top: 100px;
-    text-align: center;
+    margin-top: 110px;
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
     width: 100%;
-    height: 2em;
+    height: 1em;
+    font-family: "League Spartan";
+    font-size: 1.6rem;
+    font-weight: 600;
+    color: var(--color-primary);
+  }
+
+  .project-title .icon {
+    width: 0.9em;
+    height: 0.9em;
+    display: inline-block;
+    background-size: contain;
+  }
+
+  .icon.github {
+    background-image: url("/icons/icon-github.svg");
   }
 
   .project-description {
-    margin-top: 20px;
+    margin-top: 30px;
     text-align: center;
     width: 100%;
     height: 3.3em;
+    font-size: 0.8rem;
+    font-family: "Istok Web";
+    text-align: justify;
+    color: var(--color-primary-75-opacity);
   }
 
   .skeleton {
